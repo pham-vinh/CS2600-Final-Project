450199f48176589a8076d030b34b41b969d37772
random-debug <112684637+pham-vinh@users.noreply.github.com>
2022-12-09 18:25:00 -0800

    inital plan

--- publisher.c
+++ publisher.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+// need broker information
+// using mosquitto mqtt
+
 int main(int argc, char const *argv[])
 {
-	/* code */
+	
+	// init Mosquitto client
+
+	// will use mosquitto documentation
+	// have a while loop to listen from the broker
+
+	// testing with a key stroke to send message to esp
+	
 	return 0;
 }
