2b92339c1cecdd8f9778ec6d0b649e97c70bb889
random-debug <112684637+pham-vinh@users.noreply.github.com>
2022-12-10 03:56:19 -0800

    Update publisher.c

--- publisher.c
+++ publisher.c
@@ -1,13 +1,10 @@
-#include <mosquitto.h>
 #include <stdio.h>
+#include "C:\msys64\mingw64\include\mosquitto\devel\mosquitto.h"
 
-
-// need broker information
-const char* broker_host = "test.mosquitto.org";
+const char *broker_host = "test.mosquitto.org";
 const int broker_port = 1883;
 
-// using mosquitto mqtt
-struct mosquitto* client;
+struct mosquitto *client;
 
 int main(int argc, char const *argv[])
 {
@@ -15,24 +12,25 @@ int main(int argc, char const *argv[])
 	// init Mosquitto client
 	mosquitto_lib_init();
 
-	// will use mosquitto documentation
 	client = mosquitto_new("publisher", true, NULL);
 	mosquitto_connect(client, broker_host, broker_port, 0);
 
-
-	// have a while loop to listen from the broker
 	while(1) {
 		int result = mosquitto_loop(client, -1, 1);
 
-		if (result) {
+		if(result) {
 			fprintf(stderr, "Error: %s\n", mosquitto_strerror(result));
 			break;
 		}
 	}
-	// testing with a key stroke to send message to esp
 
-	if (kbhit() && getch() == ' ') {
-		mosquitto_publish(client, NULL, "esp32/led", 1, "1", 0, false);
+	if (getchar() == ' ') {
+		mosquitto_publish(client, NULL, "esp/led", 1, "1", 0, false);
 	}
+
+
+	mosquitto_disconnect(client);
+	mosquitto_lib_cleanup();
+
 	return 0;
 }
