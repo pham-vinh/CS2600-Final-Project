a45ede3f2c48e70c3b989490bc5dbeba1d2a5660
random-debug <112684637+pham-vinh@users.noreply.github.com>
2022-12-10 01:30:43 -0800

    imported mosquitto

--- publisher.c
+++ publisher.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
+#include "mosquitto.h"
+
 
 // need broker information
+const char* broker_host = "test.mosquitto.org";
+const int broker_port = 1883;
+
 // using mosquitto mqtt
+struct mosquitto* client;
 
 int main(int argc, char const *argv[])
 {
 	
 	// init Mosquitto client
+	mosquitto_lib_init();
 
 	// will use mosquitto documentation
+	client = mosquitto_new("publisher", true, NULL);
+	mosquitto_connect(client, broker_host, broker_port, 0);
+
+
 	// have a while loop to listen from the broker
 
 	// testing with a key stroke to send message to esp
-	
+
 	return 0;
 }
