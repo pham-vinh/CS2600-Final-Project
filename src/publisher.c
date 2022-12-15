3df8d77915974aa6e08fa685b52d0aa026d1558e
random-debug <112684637+pham-vinh@users.noreply.github.com>
2022-12-14 20:13:29 -0800

    transferring to mosquitto

--- src/publisher.c
+++ src/publisher.c
@@ -1,34 +1,51 @@
-#include "hivemq/client/mqtt_client.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define ADDRESS		"tcp://your-server-hostname-or-ip-address:1883"
-#define CLIENTID	"Publisher"
-#define TOPIC		"led"
-#define PAYLOAD		"on"
-#define QOS			1
-#define TIMEOUT		10000L
+#include "MQTTClient.h"
+
+#define ADDRESS     "tcp://broker.emqx.io:1883"
+#define CLIENTID    "emqx_test"
+#define TOPIC       "testtopic/1"
+#define PAYLOAD     "Hello World!"
+#define QOS         1
+#define TIMEOUT     10000L
 
 int main(int argc, char* argv[])
 {
-    mqtt::client* client;
-    mqtt::connect_options conn_opts;
-    mqtt::message pubmsg;
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    MQTTClient_deliveryToken token;
+    int rc;
 
-    client = new mqtt::client(ADDRESS, CLIENTID);
-    client->connect(conn_opts);
+    MQTTClient_create(&client, ADDRESS, CLIENTID,
+        MQTTCLIENT_PERSISTENCE_NONE, NULL);
+  
+    // MQTT Connection parameters
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
 
-    if (argc > 1 && strcmp(argv[1], "on") == 0) {
-        pubmsg.set_payload(PAYLOAD);
-        pubmsg.set_qos(QOS);
-        client->publish(TOPIC, pubmsg);
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to connect, return code %d\n", rc);
+        exit(-1);
     }
-
-    printf("Message '%s' with QoS %d sent to topic '%s'.\n", PAYLOAD, QOS, TOPIC);
-
-    client->disconnect();
-    delete client;
-
-    return EXIT_SUCCESS;
+  
+    // Publish message
+    pubmsg.payload = PAYLOAD;
+    pubmsg.payloadlen = strlen(PAYLOAD);
+    pubmsg.qos = QOS;
+    pubmsg.retained = 0;
+    MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+    printf("Waiting for up to %d seconds for publication of %s\n"
+            "on topic %s for client with ClientID: %s\n",
+            (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
+    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+    printf("Message with delivery token %d delivered\n", token);
+  
+    // Disconnect
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
+    return rc;
 }
---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        char num = '2';
+        char num2 = '0';
+        int test = (int)num - (int)num2;
+        System.out.println(test);
+    }
+}
