5fa71142518717f351967b19e43b2e4c7ebaa055
Vinh Pham <vinhpham@cpp.edu>
2022-12-15 18:28:56 -0800

    Revert back to mosquitto basic code to mosquitto

--- src/publisher.c
+++ src/publisher.c
@@ -1,51 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <mosquitto.h>
 
-#include "MQTTClient.h"
-
-#define ADDRESS     "tcp://broker.emqx.io:1883"
-#define CLIENTID    "emqx_test"
-#define TOPIC       "testtopic/1"
-#define PAYLOAD     "Hello World!"
-#define QOS         1
-#define TIMEOUT     10000L
 
 int main(int argc, char* argv[])
 {
-    MQTTClient client;
-    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-    MQTTClient_message pubmsg = MQTTClient_message_initializer;
-    MQTTClient_deliveryToken token;
+
     int rc;
 
-    MQTTClient_create(&client, ADDRESS, CLIENTID,
-        MQTTCLIENT_PERSISTENCE_NONE, NULL);
-  
-    // MQTT Connection parameters
-    conn_opts.keepAliveInterval = 20;
-    conn_opts.cleansession = 1;
+    struct mosquitto * mosq;
+    
+    mosquitto_lib_init();
 
-    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
-    {
-        printf("Failed to connect, return code %d\n", rc);
-        exit(-1);
+    mosq = mosquitto_new("publisher-test", true, NULL);
+    
+    rc = mosquitto_connect(mosq, "test.mosquitto.org", 1883, 60);
+
+    if (rc != 0) {
+        printf("Client could not connect to broker! Error Code: %d\n", rc);
+        mosquitto_destroy(mosq);
+        return -1;
     }
-  
-    // Publish message
-    pubmsg.payload = PAYLOAD;
-    pubmsg.payloadlen = strlen(PAYLOAD);
-    pubmsg.qos = QOS;
-    pubmsg.retained = 0;
-    MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
-    printf("Waiting for up to %d seconds for publication of %s\n"
-            "on topic %s for client with ClientID: %s\n",
-            (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
-    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
-    printf("Message with delivery token %d delivered\n", token);
-  
-    // Disconnect
-    MQTTClient_disconnect(client, 10000);
-    MQTTClient_destroy(&client);
-    return rc;
+
+    printf("We are now connected to the broker!\n");
+
+    // has to be a pointer for any type
+    mosquitto_publish(mosq, NULL, "ESP32/input", 6, "Hello", 0, false);
+    
+    mosquitto_disconnect(mosq);
+    mosquitto_destroy(mosq);
+
+    mosquitto_lib_cleanup();
+    return EXIT_SUCCESS;
 }
