375a5defcbac944e3b2481052212e608631d4843
random-debug <112684637+pham-vinh@users.noreply.github.com>
2022-12-16 02:50:56 -0800

    testing

--- src/tictactoe.c
+++ src/tictactoe.c
@@ -87,7 +87,8 @@ void on_message(struct mosquitto* mosq, void* obj, const struct mosquitto_messag
             break;
     }
 }
-void connect() {
+
+int connect() {
     int rc;
     struct mosquitto* mosq;
 
@@ -113,7 +114,28 @@ void connect() {
 }
 
 int main() {
-    connect();
+    int rc;
+    struct mosquitto* mosq;
+
+    mosquitto_lib_init();
+
+    mosq = mosquitto_new("LaptopClient", true, NULL);
+
+    rc = mosquitto_connect(mosq, "test.mosquitto.org", 1883, 10);
+
+    if (rc != 0) {
+        printf("Client could not connect to broker! Error Code: %d\n", rc);
+        mosquitto_destroy(mosq);
+        return EXIT_SUCCESS;
+    }
+    printf("We are now connected to the broker!\n");
+
+    mosquitto_publish(mosq, NULL, "ESP32/input", 6, "Hello", 0, false);
+
+    mosquitto_disconnect(mosq);
+    mosquitto_destroy(mosq);
+
+    mosquitto_lib_cleanup();
 
     displayMenu();
 
